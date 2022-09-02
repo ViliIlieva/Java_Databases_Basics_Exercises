c088c2f6e0f3a89281f14a5a38f59d359490cb8b
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-02 16:26:32 +0300

    MySQL

---
+++ 02 Data Definition and Data Types - Exercise/01CreateTables.sql
@@ -0,0 +1,9 @@
+ create table `minions`(
+id int primary key auto_increment, 
+`name` varchar(30),
+age int
+);
+create table `towns` (
+town_id int primary key auto_increment,
+`name` varchar(30)
+);
