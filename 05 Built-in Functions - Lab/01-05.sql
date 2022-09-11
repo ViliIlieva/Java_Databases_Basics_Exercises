15665fe9a4af49a091bcf7f233e607fc8a1cdcbe
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-11 16:06:40 +0300

    Update 01-05.sql

--- 05 Built-in Functions - Lab/01-05.sql
+++ 05 Built-in Functions - Lab/01-05.sql
@@ -8,6 +8,10 @@ SELECT replace(`title`, 'The', '***') FROM books
 WHERE substring(title, 1, 3) = 'The'
 ORDER BY id;
 
+3.	Sum Cost of All Books
+SELECT round(sum(`cost`), 2)
+FROM `books`;
+
 4.	Days Lived
 SELECT concat(`first_name`, ' ', `last_name`) AS 'Full Name', 
 timestampdiff(day, `born`, `died`) as 'Days Lived'
