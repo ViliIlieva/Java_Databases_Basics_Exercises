247c073c603b86c19496b6a16fab9e9c96f7ae8e
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-11 17:33:01 +0300

    Update 01-05.sql

--- 06 Built-in Functions - Exercise/01-05.sql
+++ 06 Built-in Functions - Exercise/01-05.sql
@@ -1,5 +1,10 @@
-1.	Find Names of All Employees by First Name
+1. Find Names of All Employees by First Name
 SELECT first_name, last_name from employees
 WHERE first_name LIKE 'Sa%'
+ORDER BY employee_id;
+
+2.	Find Names of All Employees by Last Name
+SELECT first_name, last_name from employees
+WHERE last_name LIKE '%ei%'
 ORDER BY employee_id;
 
