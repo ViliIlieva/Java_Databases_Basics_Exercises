5125f334becd63fd162eeefd6b15630e2225a4e2
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-11 14:53:08 +0300

    Update 01-05.sql

--- 05 Built-in Functions - Lab/01-05.sql
+++ 05 Built-in Functions - Lab/01-05.sql
@@ -1,2 +1,9 @@
+1.	Find Book Titles
 SELECT `title` FROM books
 WHERE substring(title, 1, 3) = 'The';
+
+
+2.	Replace Titles
+SELECT replace(`title`, 'The', '***') FROM books
+WHERE substring(title, 1, 3) = 'The'
+ORDER BY id;
