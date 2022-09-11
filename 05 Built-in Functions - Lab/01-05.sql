8ddba7b5a4eea6e718bcfff62aedd5740f3807b1
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-11 16:01:50 +0300

    Update 01-05.sql

--- 05 Built-in Functions - Lab/01-05.sql
+++ 05 Built-in Functions - Lab/01-05.sql
@@ -7,3 +7,8 @@ WHERE substring(title, 1, 3) = 'The';
 SELECT replace(`title`, 'The', '***') FROM books
 WHERE substring(title, 1, 3) = 'The'
 ORDER BY id;
+
+4.	Days Lived
+SELECT concat(`first_name`, ' ', `last_name`) AS 'Full Name', 
+timestampdiff(day, `born`, `died`) as 'Days Lived'
+FROM `authors`;
