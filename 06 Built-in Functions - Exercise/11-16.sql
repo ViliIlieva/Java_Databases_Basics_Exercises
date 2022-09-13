824a0c3ff125b2b181ad31bbf04afb9b8305bc0c
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-13 14:14:17 +0300

    Update 11-16.sql

--- 06 Built-in Functions - Exercise/11-16.sql
+++ 06 Built-in Functions - Exercise/11-16.sql
@@ -35,4 +35,8 @@ ELSE 'Extra Long'
 END as 'Duration'
 FROM games;
 
-16.	 Orders Table
+16.Orders Table
+SELECT product_name, order_date,
+adddate(order_date, interval 3 day) as pay_due, 
+adddate(order_date, interval 1 month) as deliver_due
+FROM orders;
