7df98fe9596b256874027389da9d7d78af06a475
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-14 20:03:18 +0300

    Update 01-10.sql

--- 08 Data Aggregation - Exercise/01-10.sql
+++ 08 Data Aggregation - Exercise/01-10.sql
@@ -20,10 +20,25 @@ ORDER BY avg(magic_wand_size)
 LIMIT 1;
 
 5.Deposits Sum
+SELECT `deposit_group`, sum(deposit_amount) as 'total_sum'
+from `wizzard_deposits`
+GROUP BY deposit_group
+ORDER BY `total_sum`;
 
 6.Deposits Sum for Ollivander Family
+SELECT `deposit_group`, sum(deposit_amount) as 'total_sum'
+from `wizzard_deposits`
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group;
 
 7.Deposits Filter
+SELECT `deposit_group`, sum(deposit_amount) as 'total_sum'
+from `wizzard_deposits`
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
 
 8.Deposit Charge
 
