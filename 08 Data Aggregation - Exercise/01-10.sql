af5d20a90c9b1dd298bb63fc64e394a1a916a1d3
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-14 20:10:35 +0300

    Update 01-10.sql

--- 08 Data Aggregation - Exercise/01-10.sql
+++ 08 Data Aggregation - Exercise/01-10.sql
@@ -41,6 +41,10 @@ HAVING `total_sum` < 150000
 ORDER BY `total_sum` DESC;
 
 8.Deposit Charge
+SELECT `deposit_group`, magic_wand_creator, min(deposit_charge) as 'min_deposit_charge'
+from `wizzard_deposits`
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY `magic_wand_creator` ASC, deposit_group;
 
 9.Age Groups
 
