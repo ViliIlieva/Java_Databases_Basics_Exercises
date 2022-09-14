b285b7d497d7be1476aae96013b7214b45c10705
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-14 17:15:34 +0300

    Update 01-10.sql

--- 08 Data Aggregation - Exercise/01-10.sql
+++ 08 Data Aggregation - Exercise/01-10.sql
@@ -1,3 +1,23 @@
 1.Records' Count
 SELECT count(*) as 'count'
 FROM `wizzard_deposits`;
+
+2.Longest Magic Wand
+SELECT max(magic_wand_size) as 'longest_magic_wand'
+FROM `wizzard_deposits`;
+
+3.Longest Magic Wand Per Deposit Groups
+
+4.Smallest Deposit Group Per Magic Wand Size*
+
+5.Deposits Sum
+
+6.Deposits Sum for Ollivander Family
+
+7.Deposits Filter
+
+8.Deposit Charge
+
+9.Age Groups
+
+10.First Letter
