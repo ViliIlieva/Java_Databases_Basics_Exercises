a726104d57a5d53aeb2e1ec58902485832788dc8
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-15 21:54:54 +0300

    Create 11-18.sql

---
+++ 08 Data Aggregation - Exercise/11-18.sql
@@ -0,0 +1,30 @@
+11.	Average Interest 
+SELECT deposit_group, is_deposit_expired, avg(deposit_interest) as 'average_interest'
+FROM `wizzard_deposits`
+WHERE deposit_start_date > '1985-01-01'
+GROUP BY deposit_group, is_deposit_expired
+ORDER BY `deposit_group` DESC, is_deposit_expired ASC;
+
+12.	 Employees Minimum Salaries
+SELECT department_id, min(salary) as 'minimum_salary'
+FROM `employees`
+WHERE department_id IN (2, 5, 7) AND date(hire_date) > 2000-01-01
+GROUP BY department_id
+ORDER BY `department_id` ASC;
+
+13.	Employees Average Salaries
+
+
+14. Employees Maximum Salaries
+
+
+15.	Employees Count Salaries
+
+
+16.	3rd Highest Salary*
+
+
+17.	 Salary Challenge**
+
+
+18.	Departments Total Salaries
