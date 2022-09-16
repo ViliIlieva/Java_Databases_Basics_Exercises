472112d90d148461b7562fe90999dbcc3fcb9646
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-16 12:20:42 +0300

    Update 11-18.sql

--- 08 Data Aggregation - Exercise/11-18.sql
+++ 08 Data Aggregation - Exercise/11-18.sql
@@ -1,18 +1,18 @@
-11.	Average Interest 
+11.Average Interest 
 SELECT deposit_group, is_deposit_expired, avg(deposit_interest) as 'average_interest'
 FROM `wizzard_deposits`
 WHERE deposit_start_date > '1985-01-01'
 GROUP BY deposit_group, is_deposit_expired
 ORDER BY `deposit_group` DESC, is_deposit_expired ASC;
 
-12.	 Employees Minimum Salaries
+12.Employees Minimum Salaries
 SELECT department_id, min(salary) as 'minimum_salary'
 FROM `employees`
 WHERE department_id IN (2, 5, 7) AND date(hire_date) > 2000-01-01
 GROUP BY department_id
 ORDER BY `department_id` ASC;
 
-13.	Employees Average Salaries
+13.Employees Average Salaries
 create table `high_paid_employees` as
 SELECT * from employees
 WHERE salary > 30000;
@@ -29,19 +29,19 @@ FROM `high_paid_employees`
 GROUP BY department_id
 ORDER BY `department_id` ASC;
 
-14. Employees Maximum Salaries
+14.Employees Maximum Salaries
 SELECT department_id, max(salary) as 'max_salary'
 FROM `employees`
 GROUP BY department_id
 HAVING max_salary NOT BETWEEN 30000 and 70000
 ORDER BY `department_id` ASC;
 
-15.	Employees Count Salaries
+15.Employees Count Salaries
 SELECT count(salary) as ''
 FROM `employees`
 WHERE manager_id IS NULL;
 
-16.	3rd Highest Salary*
+16.3rd Highest Salary*
 SELECT DISTINCT department_id,
         (
         SELECT  DISTINCT salary 
@@ -54,11 +54,11 @@ FROM    employees
 HAVING third_highest_salary IS NOT NULL
 ORDER BY department_id;
 
-17.	 Salary Challenge**
+17.Salary Challenge**
 
 
 
-18.	Departments Total Salaries
+18.Departments Total Salaries
 SELECT department_id, sum(salary)	
 FROM    employees
 GROUP BY department_id
