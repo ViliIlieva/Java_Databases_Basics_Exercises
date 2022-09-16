63efd538724658c70f8a618c9f678282a1a61b3c
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-16 09:51:45 +0300

    Update 11-18.sql

--- 08 Data Aggregation - Exercise/11-18.sql
+++ 08 Data Aggregation - Exercise/11-18.sql
@@ -13,13 +13,33 @@ GROUP BY department_id
 ORDER BY `department_id` ASC;
 
 13.	Employees Average Salaries
+create table `high_paid_employees` as
+SELECT * from employees
+WHERE salary > 30000;
 
+DELETE FROM high_paid_employees
+WHERE manager_id = 42;
+
+UPDATE high_paid_employees
+SET salary = salary + 5000
+WHERE department_id = 1;
+
+SELECT department_id, avg(salary) as 'avg_salary'
+FROM `high_paid_employees`
+GROUP BY department_id
+ORDER BY `department_id` ASC;
 
 14. Employees Maximum Salaries
-
+SELECT department_id, max(salary) as 'max_salary'
+FROM `employees`
+GROUP BY department_id
+HAVING max_salary NOT BETWEEN 30000 and 70000
+ORDER BY `department_id` ASC;
 
 15.	Employees Count Salaries
-
+SELECT count(salary) as ''
+FROM `employees`
+WHERE manager_id IS NULL;
 
 16.	3rd Highest Salary*
 
