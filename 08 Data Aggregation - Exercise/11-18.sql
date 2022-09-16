8949f2b6e932c6a4dfc03a44fc3d6c189da186b9
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-16 12:28:45 +0300

    Update 11-18.sql

--- 08 Data Aggregation - Exercise/11-18.sql
+++ 08 Data Aggregation - Exercise/11-18.sql
@@ -55,8 +55,19 @@ HAVING third_highest_salary IS NOT NULL
 ORDER BY department_id;
 
 17.Salary Challenge**
-
-
+SELECT 
+    first_name, last_name, department_id
+FROM
+    employees AS e1
+WHERE
+    salary > (SELECT 
+            AVG(salary)
+        FROM
+            employees AS e2
+        WHERE
+            e1.department_id = e2.department_id)
+ORDER BY department_id, employee_id
+LIMIT 10;
 
 18.Departments Total Salaries
 SELECT department_id, sum(salary)	
