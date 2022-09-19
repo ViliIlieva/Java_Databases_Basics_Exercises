de5e56e216fa0c91e98d5acc2c1e07725aaac111
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-19 21:45:14 +0300

    Update 01-04.sql

--- 09 Table Relations - Lab/01-04.sql
+++ 09 Table Relations - Lab/01-04.sql
@@ -12,3 +12,9 @@ CONSTRAINT fk_peaks_mountains
 FOREIGN KEY (mountain_id)
 REFERENCES mountains(id)
 );
+
+2. Trip Organization
+select campers.id,vehicles.vehicle_type, concat_ws(' ',campers.first_name, campers.last_name) as driver_name 
+from campers 
+join vehicles  on
+vehicles.driver_id = campers.id;
