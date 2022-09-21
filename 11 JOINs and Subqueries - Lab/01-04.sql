b4af8971f5254504de08924ad386c3220b543ed0
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-21 16:24:22 +0300

    Update 01-04.sql

--- 11 JOINs and Subqueries - Lab/01-04.sql
+++ 11 JOINs and Subqueries - Lab/01-04.sql
@@ -8,6 +8,17 @@ ORDER BY employee_id
 LIMIT 5;
 
 -- 2. Towns and Addresses
+SELECT a.town_id, t.name as 'town_name', a.address_text
+from addresses as a
+join towns as t
+on a.town_id = t.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY a.town_id, a.address_id;
+
+-- 3. Employees Without Managers
+SELECT employee_id, first_name, last_name, department_id, salary
+from employees
+WHERE manager_id is NULL;
 
 -- 4. High Salary
 SELECT count(*)
