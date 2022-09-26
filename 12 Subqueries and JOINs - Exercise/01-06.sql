3013e920c0d6f5cb3e8b4c53cac15919134e0ac1
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-26 17:26:26 +0300

    Update 01-06.sql

--- 12 Subqueries and JOINs - Exercise/01-06.sql
+++ 12 Subqueries and JOINs - Exercise/01-06.sql
@@ -44,3 +44,9 @@ LIMIT 3;
 
 
 -- 06. Employees Hired After
+SELECT e.first_name, e.last_name, e.hire_date, d.`name`
+FROM employees as e
+join departments as d
+ON e.department_id = d.department_id
+WHERE e.hire_date > 1999-01-01 and d.`name` = 'Sales' OR d.`name` = 'Finance'
+ORDER BY e.hire_date ASC;
