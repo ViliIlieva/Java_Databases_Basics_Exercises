9b5baa6a77435c3eb41353852b3746d2a37a3284
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-26 17:02:24 +0300

    Update 01-06.sql

--- 12 Subqueries and JOINs - Exercise/01-06.sql
+++ 12 Subqueries and JOINs - Exercise/01-06.sql
@@ -6,3 +6,41 @@ ON a.address_id = e.address_id
 ORDER BY address_id ASC
 LIMIT 5;
 
+-- 02. Addresses with Towns
+SELECT e.first_name, e.last_name, t.`name`, a.address_text
+FROM addresses as a
+join employees as e
+ON a.address_id = e.address_id
+join towns as t
+on a.town_id = t.town_id
+ORDER BY e.first_name, e.last_name
+LIMIT 5;
+
+-- 03. Sales Employee
+SELECT e.employee_id, e.first_name, e.last_name, d.`name`
+FROM employees as e
+join departments as d
+ON e.department_id = d.department_id
+WHERE d.`name` = 'Sales'
+ORDER BY e.employee_id DESC;
+
+-- 04. Employee Departments
+SELECT e.employee_id, e.first_name, e.salary, d.`name`
+FROM employees as e
+join departments as d
+ON e.department_id = d.department_id
+WHERE e.salary > 15000
+ORDER BY d.department_id DESC
+LIMIT 5;
+
+-- 05. Employees Without Project
+SELECT e.employee_id, e.first_name
+FROM employees as e
+LEFT JOIN `employees_projects` p
+on e.employee_id = p.employee_id
+WHERE p.project_id is NULL
+ORDER BY e.employee_id DESC
+LIMIT 3;
+
+
+-- 06. Employees Hired After
