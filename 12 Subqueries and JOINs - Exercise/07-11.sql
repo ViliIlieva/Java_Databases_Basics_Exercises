fdd1a211cb2e23289c582e1db5ba671d67afe332
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-27 16:47:55 +0300

    Update 07-11.sql

--- 12 Subqueries and JOINs - Exercise/07-11.sql
+++ 12 Subqueries and JOINs - Exercise/07-11.sql
@@ -7,6 +7,7 @@ WHERE DATE(p.start_date) > '2002-08-13' AND p.end_date IS NULL
 ORDER BY e.first_name ASC, p.`name` ASC
 LIMIT 5;
 
+-------------------------------------------------------------------------------------------
 
 -- 08. Employee 24
 select e.employee_id, e.first_name, 
@@ -25,6 +26,7 @@ from
 WHERE e.employee_id =  24 
 ORDER BY p.name;
 
+----------------------------------------------------------------------------------------------
 
 -- 09. Employee Manager
 SELECT e1.employee_id, e1.first_name, e1.manager_id, (
@@ -33,9 +35,8 @@ SELECT e1.employee_id, e1.first_name, e1.manager_id, (
     WHERE e1.manager_id = e2.employee_id
     ) AS manager_name
 FROM employees AS e1
-WHERE e1.manager_id IN (3, 7)
-ORDER BY e1.first_name;
 
+----------------------------------------------------------------------------------------------
 
 -- 10. Employee Summary
 SELECT e1.employee_id, 
@@ -51,3 +52,14 @@ on e1.department_id = d.department_id
 WHERE e1.manager_id is not null
 ORDER BY e1.employee_id
 LIMIT 5;
+
+----------------------------------------------------------------------------------------------
+
+-- 11. Min Average Salary
+SELECT avg(salary) AS min_avg_salary
+FROM employees
+GROUP BY department_id
+ORDER BY min_avg_salary
+LIMIT 1;
+WHERE e1.manager_id IN (3, 7)
+ORDER BY e1.first_name;
