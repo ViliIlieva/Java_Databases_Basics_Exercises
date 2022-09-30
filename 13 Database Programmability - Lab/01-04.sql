15b9296d38f32dfccf385dbd72572d036edff02b
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-09-30 12:16:45 +0300

    Update 01-04.sql

--- 13 Database Programmability - Lab/01-04.sql
+++ 13 Database Programmability - Lab/01-04.sql
@@ -42,3 +42,17 @@ end
 
 
 -- 4. Triggered
+CREATE TABLE deleted_employees (
+  `employee_id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) NOT NULL,
+  `department_id` int(10) NOT NULL,
+  `salary` decimal(19,4) NOT NULL);
+  
+ CREATE TRIGGER tr_deleted_employees AFTER DELETE
+ ON employees
+	FOR EACH ROW
+	INSERT INTO deleted_employees (first_name,last_name,middle_name,job_title,deparTment_id,salary)
+    VALUES (OLD.first_name,OLD.last_name,OLD.middle_name,OLD.job_title,OLD.deparTment_id,OLD.salary);
