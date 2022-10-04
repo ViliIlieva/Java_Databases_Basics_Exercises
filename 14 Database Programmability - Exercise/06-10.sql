dcd9cc99e1b2d70a6cadc8be1f8205dac51f9492
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-10-04 08:53:34 +0300

    Update 06-10.sql

--- 14 Database Programmability - Exercise/06-10.sql
+++ 14 Database Programmability - Exercise/06-10.sql
@@ -25,7 +25,16 @@ CALL usp_get_employees_by_salary_level('high');
 
 
 -- 07. Define Function
+delimiter ***
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+RETURNS INT
+DETERMINISTIC 
+	BEGIN
+    RETURN REGEXP_LIKE(lower(word), CONCAT('^[', lower(set_of_letters), ']+$'));
+END
+***
 
+SELECT ufn_is_word_comprised('oistmiahf', 'halves');
 ----------------------------------------------------------------------------------------------------
 
 -- 08. Find Full Name
