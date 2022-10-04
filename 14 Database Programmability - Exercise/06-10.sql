1acdb851dd0e995adca2b33fdd782c527aafc8d8
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-10-04 11:14:30 +0300

    Update 06-10.sql

--- 14 Database Programmability - Exercise/06-10.sql
+++ 14 Database Programmability - Exercise/06-10.sql
@@ -38,7 +38,36 @@ SELECT ufn_is_word_comprised('oistmiahf', 'halves');
 ----------------------------------------------------------------------------------------------------
 
 -- 08. Find Full Name
+delimiter ***
+CREATE PROCEDURE usp_get_holders_full_name ()
+BEGIN
+SELECT concat(first_name, ' ', last_name) as full_name FROM account_holders
+ORDER BY full_name, id ASC;
+END
+***
+CALL usp_get_holders_full_name;
+----------------------------------------------------------------------------------------------------
 
+--9. People with Balance Higher Than
+delimiter ***
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(`number` int)
+BEGIN
+SELECT ah.first_name, ah.last_name FROM account_holders as ah
+right JOIN accounts as a ON ah.id = a.account_holder_id
+GROUP BY first_name, last_name
+HAVING sum(a.balance) > `number`;
+END
+***
+CALL usp_get_holders_with_balance_higher_than(7000);
 ----------------------------------------------------------------------------------------------------
 
 -- 10. Future Value Function
+delimiter ***
+CREATE FUNCTION ufn_calculate_future_value (I DECIMAL(20, 4), R double, T int)
+RETURNS DECIMAL(20, 4)
+DETERMINISTIC 
+	BEGIN
+    RETURN I*pow((1+R), T);
+END
+***
+SELECT ufn_calculate_future_value('1000', '0.5', 5);
